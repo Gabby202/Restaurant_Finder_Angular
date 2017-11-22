03c10ad0ef7baeaa3d8de24f44dcf3fcd0a4f534
gabby <gabbu202@gmail.com>
2017-11-22 22:50:30 +0000

    filtered array per county

--- app/product-service/product-service.ts
+++ app/product-service/product-service.ts
@@ -5,22 +5,22 @@ export class Product {
 export class ProductService {
     getProducts(): Array<Product> {
         var products: Array<Product> = [
-            new Product('Liverpool shirt', 'Leisure wear', 45),
-            new Product('Everton shirt', 'Leisure wear', 5),
+            new Product('Liverpool shirt', 'dublin', 45),
+            new Product('Everton shirt', 'meath', 5),
+            new Product('Bugatti Chiron', 'meath', 2000000),
+            new Product('65 inch UHDTV', 'dublin', 1800),
+            new Product('Carving skis', 'kildare', 350),
+            new Product('Ski boots', 'dublin', 150),
+            new Product('Liverpool shirt', 'kildare', 45),
+            new Product('Everton shirt', 'meath', 5),
             new Product('Bugatti Chiron', 'Auto', 2000000),
-            new Product('65 inch UHDTV', 'TV/Audio', 1800),
-            new Product('Carving skis', 'Sports equipment', 350),
-            new Product('Ski boots', 'Sports equipment', 150),
-            new Product('Liverpool shirt', 'Leisure wear', 45),
-            new Product('Everton shirt', 'Leisure wear', 5),
-            new Product('Bugatti Chiron', 'Auto', 2000000),
-            new Product('65 inch UHDTV', 'TV/Audio', 1800),
-            new Product('Carving skis', 'Sports equipment', 350),
+            new Product('65 inch UHDTV', 'kildare', 1800),
+            new Product('Carving skis', ' kildare', 350),
             new Product('Ski boots', 'Sports equipment', 150)
 
         ];
-
-        
         return products;
     }
+
+
 }
